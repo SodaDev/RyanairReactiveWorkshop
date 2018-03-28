8c46ab12240d1a8d33d4a12b96f4030aa425b791
Marcin Sodkiewicz <sodkiewicz.m@gmail.com>
2018-03-28 20:28:00 +0200

    Tickets with MVC on Hystrix

--- tickets-mvc/src/main/java/io/workshop/ticketsmvc/rest/RyanairTicketsMvcController.java
+++ tickets-mvc/src/main/java/io/workshop/ticketsmvc/rest/RyanairTicketsMvcController.java
@@ -1,18 +1,20 @@
 package io.workshop.ticketsmvc.rest;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
 import io.workshop.ticketsmvc.model.RyanairTicketDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+import rx.Observable;
 
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.springframework.http.HttpMethod.GET;
 
@@ -25,13 +27,35 @@ public class RyanairTicketsMvcController {
     private ParameterizedTypeReference<List<RyanairTicketDto>> type = new ParameterizedTypeReference<List<RyanairTicketDto>>() {};
 
     @GetMapping
-    public List<RyanairTicketDto> loadTickets() {
-        return Stream.of("concert", "exhibition", "sport")
-                .map(path -> "http://localhost:8080/api/v1/" + path)
-                .parallel()
-                .map(url -> restTemplate.exchange(url, GET, null, type))
-                .map(HttpEntity::getBody)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+    public DeferredResult<List<RyanairTicketDto>> loadTickets() {
+        DeferredResult<List<RyanairTicketDto>> deferredResult = new DeferredResult<>();
+        Observable.just("concert", "exhibition", "sport")
+                .flatMap(x -> new TicketsCommand(x).toObservable())
+                .flatMapIterable(x -> x)
+                .toList()
+                .subscribe(deferredResult::setResult, deferredResult::setErrorResult);
+
+        return deferredResult;
+    }
+
+    public class TicketsCommand extends HystrixCommand<List<RyanairTicketDto>> {
+        private final String path;
+
+        public TicketsCommand(final String path) {
+            super(Setter.withGroupKey(
+                    HystrixCommandGroupKey.Factory.asKey("ticketsPool"))
+                    .andCommandKey(HystrixCommandKey.Factory.asKey("tickets" + path)));
+            this.path = path;
+        }
+
+        @Override
+        protected List<RyanairTicketDto> run() {
+            return restTemplate.exchange("http://localhost:8080/api/v1/" + path, GET, null, type).getBody();
+        }
+
+        @Override
+        protected List<RyanairTicketDto> getFallback() {
+            return Collections.emptyList();
+        }
     }
 }
